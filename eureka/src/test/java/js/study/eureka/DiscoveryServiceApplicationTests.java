0fa78ec954e60a485073c34ba51d1bc13b5f9738
js <leejaesup@naver.com>
2024-07-02 22:54:52 +0900

    Initial commit

---
+++ eureka/src/test/java/js/study/eureka/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package js.study.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
